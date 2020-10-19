2cf664d0013843361529113168ee424202f333e9
Jason Jaslow <jason@jaslow.com>
2020-10-19 12:18:10 -0400

    Initial Commit

---
+++ Assets/Scripts/Combat/AggroGroup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
+
+namespace RPG.Combat
+{
+    public class AggroGroup : MonoBehaviour
+    {
+        [SerializeField]
+        Fighter[] fighters;
+
+        [SerializeField]
+        bool activateOnStart = false;
+
+        private void Start()
+        {
+            Activate(activateOnStart);
+        }
+
+        public void Activate(bool shouldActivate)
+        {
+            foreach (Fighter f in fighters)
+            {
+                f.enabled = shouldActivate;
+
+                CombatTarget target = f.GetComponent<CombatTarget>();
+                if(target != null)
+                    target.enabled = shouldActivate;
+            }
+                
+        }
+
+    }
+}
+
+
+
+
+
